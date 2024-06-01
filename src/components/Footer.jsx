ca397a82f0db2ba99570fa3361d8af731bff9dc6
SamanAbbasi <samanabbasi.plus@gmail.com>
2024-06-01 22:54:47 +0330

    install next js sass normalize.css set globals font and style create and complete Header and Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <hr className="mb-30" />
+        <p>&copy; Headless Blog 2023</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="pt-80 pb-80">
+      <div className="container">
+        <div className="h1">Headless Blog</div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
